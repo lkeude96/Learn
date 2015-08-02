6ba3bae797f09fa469a471b70abd47a9d8873ce3
Eude Lesperance <l.keude96@gmail.com>
2015-08-02 02:41:14 -0500

    Add Other Team - XML

--- Udacity/Android Development for Beginners/CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ Udacity/Android Development for Beginners/CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -41,15 +41,18 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void addThreeForTeamA(View v) {
-        displayForTeamA(3);
+        scoreTeamA = scoreTeamA + 3;
+        displayForTeamA(scoreTeamA);
     }
 
     public void addTwoForTeamA(View v) {
-        displayForTeamA(2);
+        scoreTeamA = scoreTeamA + 2;
+        displayForTeamA(scoreTeamA);
     }
 
     public void addOneForTeamA(View v) {
-        displayForTeamA(1);
+        scoreTeamA = scoreTeamA + 1;
+        displayForTeamA(scoreTeamA);
     }
     /**
      * Displays the given score for Team A.
