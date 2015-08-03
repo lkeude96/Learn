e0b9c491cd612168397369ab0ed05ed387c072b9
Eude Lesperance <l.keude96@gmail.com>
2015-08-02 21:23:12 -0500

    Add reset button

--- Udacity/Android Development for Beginners/CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ Udacity/Android Development for Beginners/CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -42,7 +42,7 @@ public class MainActivity extends ActionBarActivity {
     }
 
     /**
-     * Increase the score for Team A by 3 points
+     * Increase the score for Team A by 3 points.
      */
     public void addThreeForTeamA(View v) {
         scoreTeamA = scoreTeamA + 3;
@@ -50,7 +50,7 @@ public class MainActivity extends ActionBarActivity {
     }
 
     /**
-     * Increase the score for Team A by 2 points
+     * Increase the score for Team A by 2 points.
      */
     public void addTwoForTeamA(View v) {
         scoreTeamA = scoreTeamA + 2;
@@ -58,7 +58,7 @@ public class MainActivity extends ActionBarActivity {
     }
 
     /**
-     * Increase the score for Team A by 1 points
+     * Increase the score for Team A by 1 points.
      */
     public void addOneForTeamA(View v) {
         scoreTeamA = scoreTeamA + 1;
@@ -66,7 +66,7 @@ public class MainActivity extends ActionBarActivity {
     }
 
     /**
-     * Increase the score for Team B by 3 points
+     * Increase the score for Team B by 3 points.
      */
     public void addThreeForTeamB(View v) {
         scoreTeamB = scoreTeamB + 3;
@@ -74,7 +74,7 @@ public class MainActivity extends ActionBarActivity {
     }
 
     /**
-     * Increase the score for Team B by 2 points
+     * Increase the score for Team B by 2 points.
      */
     public void addTwoForTeamB(View v) {
         scoreTeamB = scoreTeamB + 2;
@@ -82,12 +82,23 @@ public class MainActivity extends ActionBarActivity {
     }
 
     /**
-     * Increase the score for Team B by 1 points
+     * Increase the score for Team B by 1 points.
      */
     public void addOneForTeamB(View v) {
         scoreTeamB = scoreTeamB + 1;
         displayForTeamB(scoreTeamB);
     }
+
+    /**
+     * Resets the score for both teams back to 0.
+     */
+    public void resetScore(View v) {
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
+    }
+
     /**
      * Displays the given score for Team A.
      */
