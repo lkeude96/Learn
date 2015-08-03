a8028b90189ec9db44f2f62e281b4f2640f55db3
Eude Lesperance <l.keude96@gmail.com>
2015-08-03 12:15:34 -0500

    Defining vs Calling a Method

--- Udacity/Android Development for Beginners/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ Udacity/Android Development for Beginners/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends ActionBarActivity {
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void display(int number) {
+    private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
         quantityTextView.setText("" + number);
@@ -57,11 +57,11 @@ public class MainActivity extends ActionBarActivity {
 
     public void increment(View view) {
         quantity = quantity + 1;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     public void decrement(View view) {
         quantity = quantity - 1;
-        display(quantity);
+        displayQuantity(quantity);
     }
 }
