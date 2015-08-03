a4b1cf544329396fa33af6db239beb62be5c9fd7
Eude Lesperance <l.keude96@gmail.com>
2015-08-03 01:17:39 -0500

    Make it pretty

--- Udacity/Android Development for Beginners/CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ Udacity/Android Development for Beginners/CourtCounter/app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -16,7 +16,6 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForTeamA(8);
     }
 
     @Override
