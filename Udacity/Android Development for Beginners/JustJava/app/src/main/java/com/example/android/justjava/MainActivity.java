9eb8dd39a8218541951f816eab9106ad74adb462
Eude Lesperance <l.keude96@gmail.com>
2015-08-10 23:42:58 -0500

    The displayMessage Method

--- Udacity/Android Development for Beginners/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ Udacity/Android Development for Beginners/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -46,21 +46,21 @@ public class MainActivity extends ActionBarActivity {
                 R.id.quantity_text_view);
         quantityTextView.setText("" + numberOfCoffees);
     }
-
-    /**
+/*
+    *//**
      * This method displays the given price on the screen.
-     */
+     *//*
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
+    }*/
 
     /**
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
 
     /**
