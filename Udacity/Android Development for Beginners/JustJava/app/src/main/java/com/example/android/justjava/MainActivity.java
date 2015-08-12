f4a2cd96681a068bb2d4699def4963670c1bcbe9
Eude Lesperance <l.keude96@gmail.com>
2015-08-11 23:57:29 -0500

    Delete displayPrice Method

--- Udacity/Android Development for Beginners/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ Udacity/Android Development for Beginners/JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -46,14 +46,6 @@ public class MainActivity extends ActionBarActivity {
                 R.id.quantity_text_view);
         quantityTextView.setText("" + numberOfCoffees);
     }
-/*
-    *//**
-     * This method displays the given price on the screen.
-     *//*
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }*/
 
     /**
      * This method displays the given text on the screen.
