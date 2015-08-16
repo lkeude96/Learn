9b6686c77ffe719d4ffb2e761e77c8e8f321c0c9
Eude Lesperance <l.keude96@gmail.com>
2015-08-16 15:49:55 -0500

    Set Data on Views

--- Udacity/Android Development for Beginners/Cookies/app/src/main/java/com/example/android/cookies/MainActivity.java
+++ Udacity/Android Development for Beginners/Cookies/app/src/main/java/com/example/android/cookies/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         // TODO: Find a reference to the ImageView in the layout. Change the image.
         ImageView imageView = (ImageView) findViewById(R.id.android_cookie_image_view);
         imageView.setImageResource(R.drawable.after_cookie);
-        
+
         // TODO: Find a reference to the TextView in the layout. Change the text.
         TextView textView = (TextView) findViewById(R.id.status_text_view);
         textView.setText("I'm so full");
