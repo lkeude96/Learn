5ba4373f0f45d08929fa21cdf3707bff9616087e
Eude Lesperance <l.keude96@gmail.com>
2015-10-10 16:39:01 -0500

    Create Some Fake Data

--- Udacity/Developing Android Apps/Sunshine/app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
+++ Udacity/Developing Android Apps/Sunshine/app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
@@ -6,6 +6,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * A placeholder fragment containing a simple view.
  */
@@ -17,6 +21,19 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        String[] forecastArray = {
+                "Today - Sunny - 88/63",
+                "Tomorrow - Foggy - 70/40",
+                "Weds - Cloudy - 72/63",
+                "Thurs - Asteroids - 75/65",
+                "Fri - Heavy Rain - 65/56 ",
+                "Sat - HELP TRAPPED IN WEATHERSTATION - 60/51"
+                "Sun - Sunny - 80/68"
+        };
+
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
+        return rootView;
     }
 }
