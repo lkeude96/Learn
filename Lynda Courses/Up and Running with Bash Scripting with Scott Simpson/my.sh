d04ead9dbd056a01090bf7edcbe95ebe4688c9b1
Eude Lesperance <l.keude96@gmail.com>
2015-10-13 23:52:21 -0500

    Working with Numbers 1

--- Lynda Courses/Up and Running with Bash Scripting with Scott Simpson/my.sh
+++ Lynda Courses/Up and Running with Bash Scripting with Scott Simpson/my.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 # This is a basic bash script.
-a=$(ping -c 1 example.com | grep 'bytes from' | cut -d = -f 4)
-echo "The ping was $a"
+d=2
+e=$((d+2))
+echo $e
+((e++))
+echo $e
+((e--))
+echo $e
+echo
+((e+=5))
+echo $e
+((e*=3))
+echo $e
+((e/=3))
+echo $e
+((e-=5))
+echo $e
