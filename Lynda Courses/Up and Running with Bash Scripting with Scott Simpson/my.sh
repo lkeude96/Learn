5028362b3ab566ad38c7b7585449c1a56bf76b25
Eude Lesperance <l.keude96@gmail.com>
2015-10-13 23:58:52 -0500

    Working with Numbers 2

--- Lynda Courses/Up and Running with Bash Scripting with Scott Simpson/my.sh
+++ Lynda Courses/Up and Running with Bash Scripting with Scott Simpson/my.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 # This is a basic bash script.
-d=2
-e=$((d+2))
-echo $e
-((e++))
-echo $e
-((e--))
-echo $e
-echo
-((e+=5))
-echo $e
-((e*=3))
-echo $e
-((e/=3))
-echo $e
-((e-=5))
-echo $e
+f=$((1/3))
+echo $f
+g=$(echo 1/3 | bc -l)
+echo $g
