5d026761f7ca185ad69b2a53c59f8dc132bdddbc
Eude Lesperance <l.keude96@gmail.com>
2015-10-17 12:57:02 -0500

    Debugging

--- Coursera/iOS App Development with Swift Specialization/Introduction To Swift Programming/MyFirstApp/MyFirstApp/ViewController.swift
+++ Coursera/iOS App Development with Swift Specialization/Introduction To Swift Programming/MyFirstApp/MyFirstApp/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let image = UIImage(named: "Shift")
+        imageView.image = image
     }
 
     override func didReceiveMemoryWarning() {
