58b54b42a06d30534ea5ab8bd46e4797e6d18633
Eude Lesperance <l.keude96@gmail.com>
2015-10-17 14:36:46 -0500

    Display Content in DetailActivity

--- Udacity/Developing Android Apps/Sunshine/app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ Udacity/Developing Android Apps/Sunshine/app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.sunshine.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
@@ -8,6 +9,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class DetailActivity extends ActionBarActivity {
 
@@ -48,8 +50,13 @@ public class DetailActivity extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-
+            Intent intent = getActivity().getIntent();
             View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+            if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
+                String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+                ((TextView) rootView.findViewById(R.id.detail_text))
+                        .setText(forecastStr);
+            }
             return rootView;
         }
     }
