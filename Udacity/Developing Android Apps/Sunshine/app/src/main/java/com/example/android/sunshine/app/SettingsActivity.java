2a8a7e451c026c3cbcdf749f89c68456a5b5b33a
Eude Lesperance <l.keude96@gmail.com>
2015-10-19 00:12:22 -0500

    Modify SettingsActivity

--- Udacity/Developing Android Apps/Sunshine/app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
+++ Udacity/Developing Android Apps/Sunshine/app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
@@ -16,6 +16,11 @@ public class SettingsActivity extends PreferenceActivity implements Preference.O
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.pref_general);
+
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
+        
     }
 
     private void bindPreferenceSummaryToValue(Preference preference) {
