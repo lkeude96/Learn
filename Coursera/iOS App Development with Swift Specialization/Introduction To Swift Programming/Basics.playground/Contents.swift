a0f741a2efd14fa42363c78818aa61498c56d6d6
Eude Lesperance <l.keude96@gmail.com>
2015-10-20 23:24:42 -0500

    Simple Control Flow

--- Coursera/iOS App Development with Swift Specialization/Introduction To Swift Programming/Basics.playground/Contents.swift
+++ Coursera/iOS App Development with Swift Specialization/Introduction To Swift Programming/Basics.playground/Contents.swift
@@ -8,3 +8,13 @@ var str:String? = "Hello, playground"
 //str = nil
 
 print(str)
+
+//var counter = 0
+
+for counter in 0..<10 {
+    guard counter != 2 else { continue }
+    
+    if counter != 5 {
+        print(counter)
+    }
+}
