b58554f4b0cf426b5d3549e410d9e95eb3b6b592
Eude Lesperance <l.keude96@gmail.com>
2015-10-26 21:19:12 -0500

    Displaying the titles from the plist file in the TableView

--- TopCoder/SurveyApp/SurveyApp/ViewController.swift
+++ TopCoder/SurveyApp/SurveyApp/ViewController.swift
@@ -8,16 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var surveyTableView: UITableView!
+    
+    
+    var data: [NSDictionary] = [];
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.surveyTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        let dataPath = NSBundle.mainBundle().pathForResource("data", ofType: "plist")
+        data = NSArray(contentsOfFile: dataPath!)! as! [NSDictionary]
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) 
+        
+        cell.textLabel?.text = data[indexPath.row]["title"] as? String
+        return cell
     }
 
 
