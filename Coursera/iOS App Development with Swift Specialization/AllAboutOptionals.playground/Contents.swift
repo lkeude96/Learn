4a9b49c6cd26df8c7689ca0f65242f76e9044986
Eude Lesperance <l.keude96@gmail.com>
2015-10-30 10:15:15 -0500

    Optionals 1

---
+++ Coursera/iOS App Development with Swift Specialization/AllAboutOptionals.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var maybeString: String? = "hi"
+
+if maybeString != nil {
+    maybeString!.characters.count
+}
+
+var mostLikelyString: String! = "Hey"
+
+mostLikelyString.characters.count
