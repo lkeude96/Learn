308801ce2f5b018ea9e6f1a0da0515052545b6e2
Eude Lesperance <l.keude96@gmail.com>
2015-11-05 05:48:15 -0600

    First Look at image Processing

--- Coursera/iOS App Development with Swift Specialization/Introduction To Swift Programming/DataProcessing.playground/Contents.swift
+++ Coursera/iOS App Development with Swift Specialization/Introduction To Swift Programming/DataProcessing.playground/Contents.swift
@@ -4,3 +4,20 @@ import UIKit
 
 let image = UIImage(named: "sample")!
 
+let rgbaImage = RGBAImage(image: image)!
+
+let x = 20
+let y = 20
+
+let index = y * rgbaImage.width + x
+
+var pixel = rgbaImage.pixels[index]
+
+pixel.red = 255
+pixel.green = 0
+pixel.blue = 0
+
+
+rgbaImage.pixels[index] = pixel
+
+let newImage = rgbaImage.toUIImage()!
