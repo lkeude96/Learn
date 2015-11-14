1d533c6df7795394c90aa935e3e7144f13bb4bca
Eude Lesperance <l.keude96@gmail.com>
2015-11-14 11:49:05 -0600

    Files, naming and Class Differences

---
+++ Coursera/iOS App Development with Swift Specialization/Introduction To Swift Programming/ObjC vs Swift/ObjC vs Swift/City.swift
@@ -0,0 +1,13 @@
+//
+//  City.swift
+//  ObjC vs Swift
+//
+//  Created by Eude Kinsley Lesperance on 11/14/15.
+//  Copyright © 2015 uoft. All rights reserved.
+//
+
+import UIKit
+
+class City {
+
+}
---
+++ Coursera/iOS App Development with Swift Specialization/Introduction To Swift Programming/ObjC vs Swift/ObjC vs Swift/UTCity.h
@@ -0,0 +1,13 @@
+//
+//  UTCity.h
+//  ObjC vs Swift
+//
+//  Created by Eude Kinsley Lesperance on 11/14/15.
+//  Copyright © 2015 uoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UTCity : NSObject
+
+@end
