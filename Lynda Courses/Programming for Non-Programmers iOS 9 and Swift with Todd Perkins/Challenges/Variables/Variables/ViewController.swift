0fe8a45dcd52d7afb15325ad3350ae264cd2c921
Eude Lesperance <l.keude96@gmail.com>
2016-04-27 21:33:23 -0500

    Finished Course

---
+++ Lynda Courses/Programming for Non-Programmers iOS 9 and Swift with Todd Perkins/Challenges/Variables/Variables/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Variables
+//
+//  Created by Eude Kinsley Lesperance on 4/27/16.
+//  Copyright © 2016 Qlovur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
